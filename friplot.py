5ef64cff6c6f0ee98dd9e257a5b316df5fbecde7
Nicolas Mohnblatt <nmohnblatt@protonmail.com>
2026-05-22 10:42:32 +0100

    plot and table

--- friplot.py
+++ friplot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import csv
 import math
 import sys
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ from friudr import makeFRIUDRScheme
 from frijohnson import makeFRIJohnsonScheme
 
 
-def plot_commitment_vs_invrate(datasize_mb=32, invrates=None):
+def plot_commitment_vs_invrate(datasize_mb=32, invrates=None, export_csv=False):
     """
     Plot the commitment size of FRI UDR and FRI Johnson as a function of
     the inverse rate.
@@ -19,10 +20,12 @@ def plot_commitment_vs_invrate(datasize_mb=32, invrates=None):
     datasize_mb : int
         Data size in megabytes.
     invrates : list[int] or None
-        List of inverse rates to evaluate. Defaults to [2, 4, 8, 16, 32, 64].
+        List of inverse rates to evaluate. Defaults to [2, 4, 8, 16, 32].
+    export_csv : bool
+        If True, also write the data to commitment_vs_invrate.csv.
     """
     if invrates is None:
-        invrates = [2, 4, 8, 16, 32, 64]
+        invrates = [2, 4, 8, 16, 32]
 
     datasize = datasize_mb * 8_000_000  # convert MB to bits
 
@@ -35,6 +38,15 @@ def plot_commitment_vs_invrate(datasize_mb=32, invrates=None):
 
         scheme_johnson = makeFRIJohnsonScheme(datasize, invrate=invrate)
         com_johnson.append(scheme_johnson.com_size / 8_000)  # bits to KB
+
+    if export_csv:
+        csv_path = 'commitment_vs_invrate.csv'
+        with open(csv_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['invrate', 'com_udr_KB', 'com_johnson_KB'])
+            for invrate, udr, johnson in zip(invrates, com_udr, com_johnson):
+                writer.writerow([invrate, udr, johnson])
+        print(f'Wrote {csv_path}')
 
     fig, ax = plt.subplots(figsize=(8, 5))
     ax.plot(invrates, com_udr, marker='o', label='FRI (Unique Decoding)')
@@ -53,5 +65,9 @@ def plot_commitment_vs_invrate(datasize_mb=32, invrates=None):
 
 
 if __name__ == '__main__':
-    datasize_mb = int(sys.argv[1]) if len(sys.argv) > 1 else 32
-    plot_commitment_vs_invrate(datasize_mb)
+    opts = [opt for opt in sys.argv[1:] if opt.startswith('-')]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith('-')]
+
+    datasize_mb = int(args[0]) if args else 32
+    export_csv = '-e' in opts
+    plot_commitment_vs_invrate(datasize_mb, export_csv=export_csv)
---
+++ fritable.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+import math
+import sys
+from tabulate import tabulate
+
+from schemes import *
+from friudr import *
+from frijohnson import *
+
+
+def makeRow(name, scheme, tex):
+    comsize = '{:.2f}'.format(round(scheme.com_size/8000.0, 2))
+    encodingsize = '{:.2f}'.format(
+        round(scheme.encoding_size() / 8000000.0, 2))
+    commpqsize = '{:.2f}'.format(round(scheme.comm_per_query() / 8000.0, 2))
+    reception = scheme.reception()
+    encodinglength = scheme.encoding_length()
+    samples = scheme.samples()
+    commsize = '{:.2f}'.format(round(scheme.total_comm() / 8000000.0, 2))
+    if tex:
+        row = ["\Inst"+name, comsize, encodingsize, commpqsize, commsize]
+    else:
+        row = [name, comsize, encodingsize, commpqsize,
+               (reception, encodinglength), samples, commsize]
+    return row
+
+#####################################################################
+
+
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+if len(args) == 0:
+    print("Missing Argument: Datasize in Megabytes.")
+    print("Hint: To print the table in LaTeX code, add the option -l.")
+    sys.exit(-1)
+
+datasize = int(args[0])*8000000
+
+
+# Print to LaTeX
+tex = "-l" in opts
+
+if tex:
+    table = [["Name", "|com|", "|Encoding|", "Comm. p. Q.", "Comm Total"]]
+else:
+    table = [["Name", "|com| [KB]", "|Encoding| [MB]",
+              "Comm. p. Q. [KB]", "Reception", "Samples", "Comm Total [MB]"]]
+
+
+# scheme = makeNaiveScheme(datasize)
+# table.append(makeRow("Naive", scheme, tex))
+
+# scheme = makeMerkleScheme(datasize)
+# table.append(makeRow("Merkle", scheme, tex))
+
+# scheme = makeKZGScheme(datasize)
+# table.append(makeRow("RS", scheme, tex))
+
+# scheme = makeTensorScheme(datasize)
+# table.append(makeRow("Tensor", scheme, tex))
+
+# scheme = makeHashBasedScheme(datasize)
+# table.append(makeRow("Hash", scheme, tex))
+
+# scheme = makeHomHashBasedScheme(datasize)
+# table.append(makeRow("HomHash", scheme, tex))
+
+scheme = makeFRIUDRScheme(datasize, invrate=2)
+table.append(makeRow("FRI (UDR, 1/2)", scheme, tex))
+
+scheme = makeFRIJohnsonScheme(datasize, invrate=2)
+table.append(makeRow("FRI (Johnson, 1/2)", scheme, tex))
+
+scheme = makeFRIUDRScheme(datasize, invrate=4)
+table.append(makeRow("FRI (UDR, 1/4)", scheme, tex))
+
+scheme = makeFRIJohnsonScheme(datasize, invrate=4)
+table.append(makeRow("FRI (Johnson, 1/4)", scheme, tex))
+
+scheme = makeFRIUDRScheme(datasize, invrate=8)
+table.append(makeRow("FRI (UDR, 1/8)", scheme, tex))
+
+scheme = makeFRIJohnsonScheme(datasize, invrate=8)
+table.append(makeRow("FRI (Johnson, 1/8)", scheme, tex))
+
+scheme = makeFRIUDRScheme(datasize, invrate=16)
+table.append(makeRow("FRI (UDR, 1/16)", scheme, tex))
+
+scheme = makeFRIJohnsonScheme(datasize, invrate=16)
+table.append(makeRow("FRI (Johnson, 1/16)", scheme, tex))
+
+scheme = makeFRIUDRScheme(datasize, invrate=32)
+table.append(makeRow("FRI (UDR, 1/32)", scheme, tex))
+
+scheme = makeFRIJohnsonScheme(datasize, invrate=32)
+table.append(makeRow("FRI (Johnson, 1/32)", scheme, tex))
+
+
+if tex:
+    print(tabulate(table, headers='firstrow',
+          tablefmt='latex_raw', disable_numparse=True))
+else:
+    print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
